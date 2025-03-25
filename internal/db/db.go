95b46babf17e94847cefd861238f7aff16de7003
Adam GONCALVES <goncalvesadam@icloud.com>
2025-03-25 20:28:56 +0100

    add db with user table + model

---
+++ internal/db/db.go
@@ -0,0 +1,50 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func GetDB() *sql.DB {
+	db, err := sql.Open("sqlite3", "internal/db/database.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// SQL statement to create the users table
+	tables := `CREATE TABLE IF NOT EXISTS users (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			nickname TEXT UNIQUE,
+			age TEXT NOT NULL,
+			gender TEXT NOT NULL,
+			firstName TEXT NOT NULL,
+			lastName TEXT NOT NULL,
+			email TEXT UNIQUE NOT NULL,
+			password TEXT NOT NULL,
+			created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+		)`
+
+	// Start a transaction
+	tx, err := db.Begin()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Execute the SQL statements within the transaction
+	_, err = tx.Exec(tables)
+	if err != nil {
+		// If there's an error, roll back the transaction
+		tx.Rollback()
+		log.Fatal(err)
+	}
+
+	// If everything is okay, commit the transaction
+	err = tx.Commit()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return db
+}
