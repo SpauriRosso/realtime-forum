d92fcedacfb68b349e1a1368361874b34a01284e
anassuif <spy456@zoho.com>
2025-04-10 14:19:53 +0200

    UI : Categories
    Added categories mock code in app.js

--- static/js/elements/app.js
+++ static/js/elements/app.js
@@ -5,7 +5,10 @@ export class AppElement extends HTMLElement {
     const header = document.createElement("c-header");
     this.appendChild(header);
     // categories
+    const categories = document.createElement("c-categories");
+    this.appendChild(categories);
     // posts
+    // navbar
     const navbar = document.createElement("c-navbar");
     this.appendChild(navbar);
   }
