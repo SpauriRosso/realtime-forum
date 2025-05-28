cb3b858d5d97381a54d8078a68001fe3afecbc1f
Adam GONCALVES <goncalvesadam@icloud.com>
2025-05-28 13:52:46 +0200

    added the ability to close modals with the escape key

--- static/js/elements/modal.js
+++ static/js/elements/modal.js
@@ -1,26 +1,30 @@
 export class ModalElement extends HTMLElement {
   constructor() {
     super();
+    this.handleEscape = this.handleEscape.bind(this);
   }
 
   connectedCallback() {
     this.render();
     document.body.style.overflow = 'hidden';
+    this.addEventListener('click', (e) => this.handleClick(e));
+    document.addEventListener('keydown', this.handleEscape);
   }
 
   disconnectedCallback() {
+    document.removeEventListener('keydown', this.handleEscape);
     document.body.style.overflow = ''; // restaure scroll si tu veux bloquer scroll lors de l'ouverture modal
   }
 
-  handleOverlayClick(e) {
-    // Fermer modal si clic sur l'overlay uniquement (pas sur le contenu)
-    if (e.target === this) {
-      this.remove();
-    }
+  handleClick(e) {
+    if (e.target === this) this.remove();
+  }
+
+  handleEscape(e) {
+    if (e.code === 'Escape') this.remove();
   }
 
   render() {
     this.className = 'fixed inset-0 z-50 flex items-center justify-center bg-black/50';
-    this.addEventListener('click', this.handleOverlayClick);
   }
 }
